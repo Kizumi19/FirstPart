b5a226bdfd59fa706101a0dac50a0a44554e0c3f
Kizumi19 <juliakbgamer12@gmail.com>
2023-11-13 19:21:54 +0100

    infoMachine.py working + file result

--- cas2/Tasks/infoMachine.py
+++ cas2/Tasks/infoMachine.py
@@ -11,14 +11,34 @@ def info_sistema():
 # Informació sobre les interfícies de xarxa (IPv4, la màscara de xarxa i l'adreça MAC)
 def info_xarxa():
     interficies = netifaces.interfaces()
-    info_xarxa = []
-    for interfaç in interficies:
-        direccions = netifaces.ifaddresses
-        ipv4 = direccions.get(netifaces.AF_INET, [{}][0].get('addr','No es pot obtenir'))
-        netmask = direccions.get(netifaces.AF_INET, [{}][0].get('netmask', 'No es pot obtenir'))
-        mac = direccions.get(netifaces.AF_LINK, [{}][0].get('addr', 'No es pot obtenir'))
-        info_xarxa.append((interfaç, ipv4, netmask, mac))
+    informacio_xarxa = []
+    for interficie in interficies:
+        
+        direccions = netifaces.ifaddresses(interficie)
+        
+        ipv4_info = direccions.get(netifaces.AF_INET, [{}])
+        ipv4 = ipv4_info[0].get('addr', 'No es pot obtenir')
+        
+        netmask = ipv4_info[0].get('netmask', 'No es pot obtenir')
+        
+        mac_info = direccions.get(netifaces.AF_LINK, [{}])
+        mac = mac_info[0].get('addr', 'No es pot obtenir')
+        
+        informacio_xarxa.append((interficie, ipv4, netmask, mac))
 
-    return info_xarxa
+    return informacio_xarxa
 
 # Generar arxiu de test amb tota la informació
+def arxiu_text(nom_maquina, sistema_operatiu, info_xarxa):
+    with open(f"Resultats_{nom_maquina}.txt", "w") as arxiu:
+        arxiu.write(f"Nom màquina: {nom_maquina}\n")
+        arxiu.write(f"Sistema Operatiu: {sistema_operatiu}\n")
+        arxiu.write(f"Interfícies de xarxa:\n")
+        for interficie, ipv4, netmask, mac in info_xarxa:
+            arxiu.write(f"{interficie} - IP: {ipv4}, Màscara: {netmask}, MAC: {mac}\n")
+        
+if __name__ == "__main__":
+    nom_maquina, sistema_operatiu = info_sistema()
+    informacio_xarxa = info_xarxa()
+    arxiu_text(nom_maquina, sistema_operatiu, informacio_xarxa)
+    print(f"Text creat amb l'informació de la màquina: {nom_maquina}")
